0864cf2fb44ed04d7f6daa0210c89e55a866bfd6
Zach Cole <zcole610@gmail.com>
2018-02-11 22:40:36 -0500

    initial commit

---
+++ RNExperiment/App.js
@@ -0,0 +1,32 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+'use strict';
+import React, { Component } from 'react';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+type Props = {};
+export default class App extends Component<Props> {
+  render() {
+    return (
+      <Text style={styles.description}>Search for houses to buy! (Again)</Text>
+    );  
+  }
+}
+
+const styles = StyleSheet.create({
+  description: {
+    fontSize: 18,
+    textAlign: 'center',
+    color: '#656565',
+    marginTop: 65,
+  },
+});
